ddd5334bbbef84b6610ad775d0673a22f21387ee
Bisola <bolarinwabisola@yahoo.com>
2025-11-12 22:46:52 +0100

    railway port binding

--- frontend/server.js
+++ frontend/server.js
@@ -1,5 +1,7 @@
 // server.js
-const serve = require('serve');
-const port = process.env.PORT || 3000; // Railway sets process.env.PORT
-serve('dist', { port });
+import serve from "serve";
+
+const port = process.env.PORT || 8080;  // Railway injects PORT automatically
+const server = serve("dist", { port });
+
 console.log(`Server running on port ${port}`);
